5392f9d3544198b24e43c3791cedfeac702132a1
tahsin-s <sarkert@mcmaster.ca>
2026-08-14 09:47:11 -0400

    remove unused lines

--- src/main/java/com/modernizemainframe/api/CreateCustomerClient.java
+++ src/main/java/com/modernizemainframe/api/CreateCustomerClient.java
@@ -1,12 +1,10 @@
 package com.modernizemainframe.api;
 
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class CreateCustomerClient implements AuthorizedContentRequest {
 
     private static final String CREATE_CUSTOMER_ENDPOINT = "/v1/customers";
-    private static final String VALID_TOKEN = "valid-inqacc-inquirer-token";
 
     public Response createCustomer(Object requestBody) {
         return authorizedContentRequest()
